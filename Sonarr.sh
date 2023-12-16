a712e49a1cb9a293fbf759bcf611ce6be0b4aed7
badi713 <134585937+badi713@users.noreply.github.com>
2023-12-16 22:17:33 +0200

    Added Sonarr

--- Sonarr.sh
+++ Sonarr.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
-# Creating the jail for Radarr. Change the name of the jail
-JAIL="Radarr"
+# Creating the jail for Sonarr. Change the name of the jail
+JAIL="Sonarr"
 iocage create -n $JAIL -r 13.2-RELEASE interfaces="vnet0:bridge0" defaultrouter="none" vnet="on" dhcp="on" bpf="yes" allow_raw_sockets="1" allow_mlock="1" boot="on"
 iocage update $JAIL
+iocage set enforce_statfs=1 $JAIL
 
 # Updating the source to the latest repo
 iocage exec $JAIL "mkdir -p /usr/local/etc/pkg/repos"
 iocage exec $JAIL "echo 'FreeBSD: {url: \"pkg+http://pkg.FreeBSD.org/\${ABI}/latest\"}' > /usr/local/etc/pkg/repos/FreeBSD.conf"
 iocage exec $JAIL pkg update
 
-# Installing radarr and dependencies
-iocage exec $JAIL pkg install -y radarr
+# Installing Sonarr and dependencies
+iocage exec $JAIL pkg install -y sonarr
 
 # Mounting storage and config
 iocage exec $JAIL mkdir -p /config
-iocage exec $JAIL mkdir -p /media/Movies
+iocage exec $JAIL mkdir -p /media/Shows
 iocage exec $JAIL mkdir -p /media/Torrents
 iocage exec $JAIL mkdir -p /media/Import
 mkdir /mnt/Tank/Backup/Jailconfig/$JAIL
 iocage fstab -a $JAIL /mnt/Tank/Backup/Jailconfig/$JAIL /config nullfs rw 0 0
-iocage fstab -a $JAIL /mnt/Tank/Movies /media/Movies nullfs rw 0 0
+iocage fstab -a $JAIL /mnt/Tank/Shows /media/Shows nullfs rw 0 0
 iocage fstab -a $JAIL /mnt/Tank/Torrents /media/Torrents nullfs rw 0 0
 iocage fstab -a $JAIL /mnt/Tank/Import /media/Import nullfs rw 0 0
 
-# Creating multimedia group and add radarr user to group
+# Creating multimedia group and add Sonarr user to group
 iocage exec $JAIL "pw groupadd multimedia -g 816"
-iocage exec $JAIL "pw usermod radarr -G multimedia"
+iocage exec $JAIL "pw usermod sonarr -G multimedia"
 
 # Changing ownership to folders
-iocage exec $JAIL chown -R radarr:multimedia /config
-#iocage exec $JAIL chown -R radarr:multimedia /media/Movies
-#iocage exec $JAIL chown -R radarr:multimedia /media/Torrents
-#iocage exec $JAIL chown -R radarr:multimedia /media/Import
+iocage exec $JAIL chown -R sonarr:multimedia /config
+#iocage exec $JAIL chown -R sonarr:multimedia /media/Shows
+#iocage exec $JAIL chown -R sonarr:multimedia /media/Torrents
+#iocage exec $JAIL chown -R sonarr:multimedia /media/Import
 
 
-#iocage exec radarr vi /usr/local/etc/rc.d/radarr
-iocage exec $JAIL sysrc "radarr_enable=TRUE"
-#iocage exec $JAIL sysrc radarr_user=radarr
-iocage exec $JAIL sysrc radarr_group=multimedia
-iocage exec $JAIL sysrc radarr_data_dir="/config"
-iocage exec $JAIL service radarr start
+#iocage exec sonarr vi /usr/local/etc/rc.d/sonarr
+iocage exec $JAIL sysrc "sonarr_enable=TRUE"
+#iocage exec $JAIL sysrc sonarr_user=sonarr
+iocage exec $JAIL sysrc sonarr_group=multimedia
+iocage exec $JAIL sysrc sonarr_data_dir="/config"
+iocage exec $JAIL service sonarr start
