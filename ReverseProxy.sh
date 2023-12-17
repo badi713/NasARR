a862093e292d92e1f2ec0b08f200de6b902113b8
badi713 <134585937+badi713@users.noreply.github.com>
2023-12-17 13:23:10 +0200

    security/py-certbot-nginx

--- ReverseProxy.sh
+++ ReverseProxy.sh
@@ -7,15 +7,16 @@ iocage update $JAIL
 # Updating the source to the latest repo
 iocage exec $JAIL "mkdir -p /usr/local/etc/pkg/repos"
 iocage exec $JAIL "echo 'FreeBSD: {url: \"pkg+http://pkg.FreeBSD.org/\${ABI}/latest\"}' > /usr/local/etc/pkg/repos/FreeBSD.conf"
-iocage exec $JAIL pkg update
+#iocage exec $JAIL pkg update
 
 # Installing nginx and dependencies
 iocage exec $JAIL pkg install -y nginx security/py-certbot-nginx
 
 # Mounting storage and config
 #iocage exec $JAIL mkdir -p /usr/local/etc/nginx
-#mkdir /mnt/Tank/Backup/Jailconfig/$JAIL
-#iocage fstab -a $JAIL /mnt/Tank/Backup/Jailconfig/$JAIL /usr/local/etc/nginx nullfs rw 0 0
+mkdir /mnt/Tank/Backup/Jailconfig/$JAIL
+iocage fstab -a $JAIL /mnt/Tank/Backup/Jailconfig/$JAIL /usr/local/etc/nginx nullfs rw 0 0
+
 
 
 # Changing ownership to folders
