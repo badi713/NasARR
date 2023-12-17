297d5513192d68e2f3ecdde07fba8f9089797ee1
badi713 <134585937+badi713@users.noreply.github.com>
2023-12-17 08:26:34 +0200

    ReverseProxy.sh

--- add.sh
+++ add.sh
@@ -4,3 +4,5 @@ wget https://github.com/badi713/NasARR/raw/main/Radarr.sh -O Radarr.sh --backups
 chmod u+x Radarr.sh
 wget https://github.com/badi713/NasARR/raw/main/Sonarr.sh -O Sonarr.sh --backups=0
 chmod u+x Sonarr.sh
+wget https://github.com/badi713/NasARR/raw/main/ReverseProxy.sh -O ReverseProxy.sh --backups=0
+chmod u+x ReverseProxy.sh
