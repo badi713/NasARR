00b09ba1fe863bd01251085515fad93138c38efd
badi713 <134585937+badi713@users.noreply.github.com>
2023-12-17 17:02:30 +0200

    Bazarr.sh

--- add.sh
+++ add.sh
@@ -4,5 +4,7 @@ wget https://github.com/badi713/NasARR/raw/main/Radarr.sh -O Radarr.sh --backups
 chmod u+x Radarr.sh
 wget https://github.com/badi713/NasARR/raw/main/Sonarr.sh -O Sonarr.sh --backups=0
 chmod u+x Sonarr.sh
+wget https://github.com/badi713/NasARR/raw/main/Bazarr.sh -O Bazarr.sh --backups=0
+chmod u+x Bazarr.sh
 wget https://github.com/badi713/NasARR/raw/main/ReverseProxy.sh -O ReverseProxy.sh --backups=0
 chmod u+x ReverseProxy.sh
