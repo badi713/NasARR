bc1517eaa1dba970d73637437103e93b2effd4a8
badi713 <134585937+badi713@users.noreply.github.com>
2023-12-25 23:45:11 +0200

    bazarr.py

--- Bazarr.sh
+++ Bazarr.sh
@@ -2,25 +2,30 @@
 # Creating the jail for Bazarr. Change the name of the jail
 JAIL="Bazarr"
 iocage create -n $JAIL -r 13.2-RELEASE interfaces="vnet0:bridge0" defaultrouter="none" vnet="on" dhcp="on" bpf="yes" allow_raw_sockets="1" allow_mlock="1" boot="on"
-#iocage update $JAIL
+iocage update $JAIL
 iocage set enforce_statfs=1 $JAIL
 
 # Updating the source to the latest repo
 iocage exec $JAIL "mkdir -p /usr/local/etc/pkg/repos"
 iocage exec $JAIL "echo 'FreeBSD: {url: \"pkg+http://pkg.FreeBSD.org/\${ABI}/latest\"}' > /usr/local/etc/pkg/repos/FreeBSD.conf"
-#iocage exec $JAIL pkg update
+iocage exec $JAIL pkg update
 
-# Installing bazarr and dependencies
-iocage exec $JAIL pkg install -y bazarr
+# Installing bazarr and dependencies - not found in freshports
+#iocage exec $JAIL pkg install -y bazarr
+iocage exec $JAIL pkg install -y git python3.9 unrar ffprobe py39-webrtcvad py39-sqlite3 py39-pillow py39-numpy py39-lxml
 
 # Mounting storage and config
-iocage exec $JAIL mkdir -p /usr/local/bazarr
+iocage exec $JAIL mkdir -p /usr/local
 iocage exec $JAIL mkdir -p /mnt/Movies
 iocage exec $JAIL mkdir -p /mnt/Shows
 mkdir /mnt/Tank/Backup/Jailconfig/$JAIL
+iocage exec $JAIL "cd /usr/local"
+iocage exec $JAIL "git clone https://github.com/morpheus65535/bazarr.git"
 iocage fstab -a $JAIL /mnt/Tank/Backup/Jailconfig/$JAIL /usr/local/bazarr nullfs rw 0 0
 iocage fstab -a $JAIL /mnt/Tank/Movies /mnt/Movies nullfs rw 0 0
 iocage fstab -a $JAIL /mnt/Tank/Shows /mnt/Shows nullfs rw 0 0
+iocage exec $JAIL "cd bazarr"
+iocage exec $JAIL "python3.9 bazarr.py"
 
 # Creating multimedia group and add bazarr user to group multimedia in jail
 iocage exec $JAIL "pw groupadd multimedia -g 816"
