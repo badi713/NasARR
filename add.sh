fa003dd3de260e2b05437784aa532b2b81c7c9b8
badi713 <134585937+badi713@users.noreply.github.com>
2024-06-08 13:45:05 +0300

    Update add.sh
    added one line

--- add.sh
+++ add.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # Downloading the scripts
-wget https://github.com/badi713/NasARR/raw/main/Radarr.sh -O Radarr.sh --backups=0
-chmod u+x Radarr.sh
-wget https://github.com/badi713/NasARR/raw/main/Sonarr.sh -O Sonarr.sh --backups=0
-chmod u+x Sonarr.sh
-wget https://github.com/badi713/NasARR/raw/main/Bazarr.sh -O Bazarr.sh --backups=0
-chmod u+x Bazarr.sh
-wget https://github.com/badi713/NasARR/raw/main/ReverseProxy.sh -O ReverseProxy.sh --backups=0
-chmod u+x ReverseProxy.sh
-wget https://github.com/badi713/NasARR/raw/main/Jellyfin.sh -O Jellyfin.sh --backups=0
-chmod u+x Jellyfin.sh
+wget https://github.com/badi713/NasARR/raw/main/Radarr.sh -O Radarr.sh --backups=0 |chmod u+x Radarr.sh
+wget https://github.com/badi713/NasARR/raw/main/Sonarr.sh -O Sonarr.sh --backups=0 | chmod u+x Sonarr.sh
+wget https://github.com/badi713/NasARR/raw/main/Bazarr.sh -O Bazarr.sh --backups=0 | chmod u+x Bazarr.sh
+wget https://github.com/badi713/NasARR/raw/main/ReverseProxy.sh -O ReverseProxy.sh --backups=0 | chmod u+x ReverseProxy.sh
+wget https://github.com/badi713/NasARR/raw/main/Jellyfin.sh -O Jellyfin.sh --backups=0 | chmod u+x Jellyfin.sh
